d80b4a96f6d3e0388296b364bf78645941181ce0
MariosMoustakidis <moustakidismarios@gmail.com>
2024-06-26 14:27:52 +0300

    FINAL 3.0

--- app/src/main/java/com/example/hci_app_2024/MenuActivity.java
+++ app/src/main/java/com/example/hci_app_2024/MenuActivity.java
@@ -1,20 +1,15 @@
 package com.example.hci_app_2024;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.hci_app_2024.CallsActivity;
-import com.example.hci_app_2024.MessagesActivity;
-import com.example.hci_app_2024.RemindersActivity;
-import com.example.hci_app_2024.SOSActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private Button callsButton, messagesButton, remindersButton, sosButton;
+    private LinearLayout callsButton, messagesButton, remindersButton, sosButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +20,6 @@ public class MenuActivity extends AppCompatActivity {
         messagesButton = findViewById(R.id.messagesButton);
         remindersButton = findViewById(R.id.remindersButton);
         sosButton = findViewById(R.id.sosButton);
-
-        ImageButton microphoneButton = findViewById(R.id.button_microphone);
-
-        microphoneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, VoiceRecognitionActivity.class);
-                startActivity(intent);
-            }
-        });
 
         callsButton.setOnClickListener(new View.OnClickListener() {
             @Override
